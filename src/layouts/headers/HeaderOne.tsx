6ce1ef0a14a4c56981fe7170158ea985a3559204
MUNNA KUMAR <munnapandit2002@gmail.com>
2026-04-07 20:28:37 +0530

    Header Background Blue Color Remove

--- src/layouts/headers/HeaderOne.tsx
+++ src/layouts/headers/HeaderOne.tsx
@@ -75,7 +75,7 @@ export default function HeaderOne() {
                   <div className="tv-header-logo">
                     <Link to="/">
                       <img
-                        src="/assets/img/logo/AtiSunyaLogo.png"
+                        src="/assets/img/logo/AtiSunya.png"
                         alt="AtiSunya-logo"
                       />
                     </Link>
